682df867bbc62ec88b24ee6a519bf5f38a0eb445
Julita <julia.franchi7@gmail.com>
2023-11-29 17:35:49 -0300

    feact-Title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Title } from './components/title';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="bg-gray-800 min-h-screen text-gray-300 flex items-center justify-center py-20 px-5">
+			<div className="container flex-col max-w-xl">
+				<Title />
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/title/Title.jsx
@@ -0,0 +1,8 @@
+const Title = () => {
+	return (
+		<h1 className="text-5xl font-anton font-bold text-yellow-600">
+			To Do List{' '}
+		</h1>
+	);
+};
+export { Title };
