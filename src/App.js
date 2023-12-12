477fe2709d0b3b0446678bf322440311ad3dfb7d
Julita <julia.franchi7@gmail.com>
2023-12-12 11:16:38 -0300

    feact todo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import { ToDo } from './components/ToDo';
 import { ToDoInput } from './components/ToDoInput';
 import { ToDoList } from './components/ToDoList';
 import { Title } from './components/title';
 
 function App() {
 	return (
-		<div className="bg-gray-800 min-h-screen text-gray-300 flex items-center justify-center py-20 px-5">
+		<div className="bg-gray-800 min-h-screen h-full font-inter text-gray-300 flex items-center justify-center py-20 px-5">
 			<div className="container flex-col max-w-xl">
 				<Title />
 				<ToDoInput />
 				<ToDoList>
 					{/*si uitilizo prop con PALABRA REESERVADA CHILDREN podemos ==> */}
-					<h2>info que ira al todolist</h2>
+					<ToDo />
+					<ToDo />
+					<ToDo />
+					<ToDo />
+					<ToDo />
 				</ToDoList>
 			</div>
 		</div>
