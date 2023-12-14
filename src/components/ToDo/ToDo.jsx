1661e2cc1769e6e8d07e45e28b8d848a4a51e68e
Julita <julia.franchi7@gmail.com>
2023-12-14 15:45:39 -0300

    buttons logic with E6 and css tailwind

--- src/components/ToDo/ToDo.jsx
+++ src/components/ToDo/ToDo.jsx
@@ -4,8 +4,15 @@ const ToDo = ({ todo }) => {
 	return (
 		<div className="flex items-center  justify-between p-4 bg-gray-700 border-b border-solid border-gray-600">
 			<div className="flex items-center">
-				<span className="border border-gray-500 rounded-full p-3 cursor-pointer"></span>
-				<p className="pl-3">{title}</p>
+				{completed ? (
+					<div className="bg-green-700 p-1 rounded-full coursor-pointer">
+						<img className="h-4 w-4" src="'/check-icon.svg" alt="check icon" />
+					</div>
+				) : (
+					<span className="border border-gray-500 rounded-full p-3 cursor-pointer"></span>
+				)}
+
+				<p className={'pl-3' + (completed && 'line-through')}>{title}</p>
 			</div>
 			<img
 				className="h-5 w-5 cursor-pointer transition-all duration-300 ease-in"
@@ -16,3 +23,5 @@ const ToDo = ({ todo }) => {
 	);
 };
 export { ToDo };
+// classname con {"" + ()} es una clase DINAMICA
+//line trought es la linea de 'tachado'
