85854ae07820d7a9f7c2da0dbb168f935e85a43c
Julita <julia.franchi7@gmail.com>
2023-12-14 15:07:40 -0300

    Todo Filters view completed

--- src/components/TodoFilters/TodoFilters.jsx
+++ src/components/TodoFilters/TodoFilters.jsx
@@ -14,6 +14,9 @@ const TodoFilters = () => {
 				<FilterButton action={() => {}} active="All" filter="Active" />
 				<FilterButton action={() => {}} active="All" filter="Completed" />
 			</FilterButtonContainer>
+			<button className="text-gray-400 hover:text-white cursor-pointer transition-all duration-300 ease-in">
+				Clear Completed
+			</button>
 		</FilterContainer>
 	);
 };
